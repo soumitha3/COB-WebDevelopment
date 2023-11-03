ebd2c20924bcad96ea98d00536b8c617c0aaf65b
soumitha3 <117661217+soumitha3@users.noreply.github.com>
2023-11-03 21:56:18 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,112 @@
+let questions = [
+  {
+  numb: 1,
+  question: "Which of the following countries is the largest by land area?",
+  answer: "Russia",
+  options: [
+    "Russia",
+    "China",
+    "Canada",
+    "United States"
+  ]
+},
+  {
+  numb: 2,
+  question: "Which mountain is the tallest in the world?",
+  answer: "Mount Everest",
+  options: [
+    "Mount Kilimanjaro",
+    "Mount McKinley",
+    "Mount Everest",
+    "Mount Fuji"
+  ]
+},
+  {
+  numb: 3,
+  question: "What is the capital of Canada?",
+  answer: "Ottawa",
+  options: [
+    "Toronto",
+    "Montreal",
+    "Ottawa",
+    "Vancouver"
+  ]
+},
+  {
+  numb: 4,
+  question: "The longest river in Europe is the:",
+  answer: "Volga",
+  options: [
+    "Rhine",
+    "Danube",
+    "Seine",
+    "Volga"
+  ]
+},
+  {
+  numb: 5,
+  question: "The Amazon River flows through which continent?",
+  answer: "South America",
+  options: [
+    "Africa",
+    "Asia",
+    "North America",
+    "South America"
+  ]
+},
+{
+  numb: 6,
+  question: "The Suez Canal connects the Mediterranean Sea to which body of water?",
+  answer: "Red Sea",
+  options: [
+    "Indian Ocean",
+    "Red Sea",
+    "Black Sea",
+    "Caspian Sea"
+  ]
+},
+{
+  numb: 7,
+  question: "Which African country is known as the PEARL OF AFRICA?",
+  answer: "Uganda",
+  options: [
+    "Nigeria",
+    "Kenya",
+    "Uganda",
+    "Tanzania"
+  ]
+},
+{
+  numb: 8,
+  question: "What is the largest freshwater lake in North America?",
+  answer: "Lake Superior",
+  options: [
+    "Lake Superior",
+    "Lake Michigan",
+    "Lake Huron",
+    "Lake Erie"
+  ]
+},
+{
+  numb: 9,
+  question: "What's the world largest island by area?",
+  answer: "Greenland",
+  options: [
+    "Greenland",
+    "Madagascar",
+    "Borneo",
+    "Sri Lanka"
+  ]
+},
+{
+  numb: 10,
+  question: "Which U.S state is known as the SUNSHINE STATE?",
+  answer: "Florida",
+  options: [
+    "California",
+    "Texas",
+    "Florida",
+    "Hawali"
+  ]
+},
+];
